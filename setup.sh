18b0ff5cc60c573d4589969761b1bdca32f77fff
Adam Sheehan <a.t.sheehan@gmail.com>
2013-11-30 14:48:57 -0500

    Add config for GNU screen and xsession.
    The xsession config will run the awesome window manager on
    startup.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ ln -sf $dotfiles_dir/sbtconfig $HOME/.sbtconfig
 ln -sf $dotfiles_dir/gitconfig $HOME/.gitconfig
 ln -sf $dotfiles_dir/gitignore $HOME/.gitignore
 ln -sf $dotfiles_dir/emacs $HOME/.emacs
+ln -sf $dotfiles_dir/screenrc $HOME/.screenrc
+ln -sf $dotfiles_dir/xsession $HOME/.xsession
