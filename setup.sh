d68f88a58d54463797a2adf0b154ea4aa4268ac9
Adam Sheehan <a.t.sheehan@gmail.com>
2014-05-06 20:57:08 -0400

    Add default settings for the Awesome WM.

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ ln -sf $dotfiles_dir/emacs $HOME/.emacs
 ln -sf $dotfiles_dir/screenrc $HOME/.screenrc
 ln -sf $dotfiles_dir/xsession $HOME/.xsession
 ln -sf $dotfiles_dir/zshrc $HOME/.zshrc
+
+mkdir -p $HOME/.config
+ln -sf $dotfiles_dir/awesome $HOME/.config/awesome
