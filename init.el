ebea7788ea533277950005b0a82da316b3006ad1
Adam Sheehan <a.t.sheehan@gmail.com>
2025-08-08 19:46:23 -0400

    Configure tree-sitter grammars

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 
 (use-package markdown-mode
+  :ensure t
   :config
   (define-key markdown-mode-map (kbd "C-c C-e") #'markdown-do)
   :mode ("README\\.md\\'" . gfm-mode)
@@ -20,4 +21,20 @@
 
 (use-package magit
   :ensure t
-  :hook (git-commit-mode . (setq fill-column 72)))
+  :hook
+  (git-commit-mode . (lambda () (setq fill-column 72))))
+
+(setq treesit-language-source-alist
+      '((json "https://github.com/tree-sitter/tree-sitter-json")
+        (rust "https://github.com/tree-sitter/tree-sitter-rust")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+(dolist (lang treesit-language-source-alist)
+  (unless (treesit-language-available-p (car lang))
+    (treesit-install-language-grammar (car lang))))
+
+(add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.toml\\'" . toml-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
